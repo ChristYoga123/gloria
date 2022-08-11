4619da859d487600a2da68086aaa14393ced305d
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-08-11 10:41:53 +0700

    fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-out-up': {
+            'from': {
+                opacity: '1',
+                transform: 'translateY(0px)'
+            },
+            'to': {
+                opacity: '0',
+                transform: 'translateY(10px)'
+            },
+        }
+    },
+    animation: {
+        'fade-out-up': 'fade-out-up 0.5s ease-out'
+    }
+    },
   },
   plugins: [require('daisyui')],
 }
